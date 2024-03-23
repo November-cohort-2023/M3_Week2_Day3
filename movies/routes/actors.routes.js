f1f704fbc718ca07cdd7094fae056ff73935e920
Omar <omaraakamal@gmail.com>
2024-03-23 18:50:08 +0400

    created movies routes

--- movies/routes/actors.routes.js
+++ movies/routes/actors.routes.js
@@ -1,9 +1,21 @@
+const Actor = require('../models/Actor.model')
+
 const router = require('express').Router()
 
 
 
 router.get('/',(req,res)=>{
     res.json("actors")
+})
+
+
+router.post('/actors',(req,res)=>{
+
+    Actor.create(req.body)
+    .then(createdActor=>{
+
+        res.json(createdActor)
+    })
 })
 
 
